edd9cd3d72f2defbe33d78784c8140d13582be0e
Simon-Raphael Fischer <50429899+Kelrugem@users.noreply.github.com>
2023-04-23 22:39:53 +0800

    Patch 25 April

--- scripts/manager_combat2.lua
+++ scripts/manager_combat2.lua
@@ -944,14 +944,14 @@ function parseAttackLine(rActor, sLine)
 				local aClausesDamage = {};
 				local nIndexDamage = 1;
 				local nStartDamage, nEndDamage;
-				while nIndexDamage < #sDamage do
-					nStartDamage, nEndDamage = string.find(sDamage, ' plus ', nIndexDamage);
+				while nIndexDamage <= #sDamage do
+					nStartDamage, nEndDamage = sDamage:find(' plus ', nIndexDamage);
 					if nStartDamage then
-						table.insert(aClausesDamage, string.sub(sDamage, nIndexDamage, nStartDamage - 1));
+						table.insert(aClausesDamage, sDamage:sub(nIndexDamage, nStartDamage - 1));
 						nIndexDamage = nEndDamage;
 					else
-						table.insert(aClausesDamage, string.sub(sDamage, nIndexDamage));
-						nIndexDamage = #sDamage;
+						table.insert(aClausesDamage, sDamage:sub(nIndexDamage));
+						nIndexDamage = #sDamage + 1;
 					end
 				end
 
