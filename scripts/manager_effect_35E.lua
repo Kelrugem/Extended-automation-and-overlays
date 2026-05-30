2652e6c8d650d02490014bda85c27e052b7ef4d2
rogervinc <97792611+rogervinc@users.noreply.github.com>
2026-05-30 18:44:35 +0100

    Compatibility with Advanced Effects
    now via call to the "onCheckEffectApply" mechanism which Advanced Effects has been updated to hook into

--- scripts/manager_effect_35E.lua
+++ scripts/manager_effect_35E.lua
@@ -432,6 +432,23 @@ function evalEffect(rActor, s, nodeSpellClass)
 
 	return sOutput;
 end
+
+function getCheckDataForEffect(rActor, v)
+	local sEffectTag = DB.getValue(v, "label", "");
+	local tCheckData = EffectQueryManager.checkActorData(rActor, sEffectTag, false, v);
+	for k,effectData in pairs(tCheckData.tEffectsData) do
+		--Debug.console("getEffectsByType effectData ", k, effectData);
+		for _, comp in pairs(effectData.tComps) do
+			--Debug.console("getEffectsByType comp ", comp);
+			if comp.node == v then
+				--Debug.console("getEffectsByType found matching effect ", k);
+				tCheckData.nEffectCheck = k;
+			end
+		end
+	end
+	return tCheckData;
+end
+
 -- KEL add tags
 function getEffectsByType(rActor, sEffectType, aFilter, rFilterActor, bTargetedOnly, rEffectSpell)
 	if not rActor then
@@ -467,23 +484,12 @@ function getEffectsByType(rActor, sEffectType, aFilter, rFilterActor, bTargetedO
 	for _,v in ipairs(aEffects) do
 		-- Check active
 		local nActive = DB.getValue(v, "isactive", 0);
-		local sEffectTag = DB.getValue(v, "label", "");
 
 		-- COMPATIBILITY FOR ADVANCED EFFECTS
 		-- to add support for AE in other extensions, make this change
 		-- Check effect is from used weapon.
 		-- original line: if nActive ~= 0 then
-		local tCheckData = EffectQueryManager.checkActorData(rActor, sEffectTag, false, v);
-		for k,effectData in pairs(tCheckData.tEffectsData) do
-			Debug.console("getEffectsByType effectData ", k, effectData);
-			for _, comp in pairs(effectData.tComps) do
-				Debug.console("getEffectsByType comp ", comp);
-				if comp.node == v then
-					Debug.console("getEffectsByType found matching effect ", k);
-					tCheckData.nEffectCheck = k;
-				end
-			end
-		end
+		local tCheckData = getCheckDataForEffect(rActor, v);
 
 		if (GameManager.callFunction("onEffectCheckApply", tCheckData)) then
 		-- END COMPATIBILITY FOR ADVANCED EFFECTS
@@ -869,9 +875,9 @@ function hasEffect(rActor, sEffect, rTarget, bTargetedOnly, bIgnoreEffectTargets
 		-- COMPATIBILITY FOR ADVANCED EFFECTS
 		-- to add support for AE in other extensions, make this change
 		-- original line: if nActive ~= 0 then
-		--Debug.console("hasEffect for " .. sEffect .. " against " .. DB.getValue(v, "label", "") .. " with active state of " .. nActive);
-		--Debug.console("rActor ", rActor, "v ", v);
-		if ((not AdvancedEffects and nActive ~= 0) or (AdvancedEffects and ActionsManagerD20AE.isValidCheckEffect(rActor,v))) then
+		local tCheckData = getCheckDataForEffect(rActor, v);
+
+		if (GameManager.callFunction("onEffectCheckApply", tCheckData)) then
 		-- END COMPATIBILITY FOR ADVANCED EFFECTS
 
 			-- Parse each effect label
@@ -1081,7 +1087,10 @@ function checkConditionalHelper(rActor, sEffect, rTarget, aIgnore, rEffectSpell)
 		-- original line: if nActive ~= 0 and not StringManager.contains(aIgnore, v.getPath()) then
 		--Debug.console("checkConditionalHelper for " .. sEffect .. " against " .. DB.getValue(v, "label", "") .. " with active state of " .. nActive);
 		--Debug.console("rActor ", rActor, "v ", v);
-		if ((not AdvancedEffects and nActive ~= 0) or (AdvancedEffects and ActionsManagerD20AE.isValidCheckEffect(rActor,v))) and not StringManager.contains(aIgnore, v.getPath()) then
+		local tCheckData = getCheckDataForEffect(rActor, v);
+			--Debug.console("checkConditionalHelper tCheckData ", tCheckData);
+
+		if (GameManager.callFunction("onEffectCheckApply", tCheckData)) then
 		-- END COMPATIBILITY FOR ADVANCED EFFECTS
 		
 			-- Parse each effect label
